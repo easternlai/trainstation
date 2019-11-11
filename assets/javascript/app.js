83b13c396cdf07bfae4104b5d9ed12892844e783
Eastern Lai <easternlai@hotmail.com>
2019-11-10 22:39:21 -0800

    Working app, needs CSS work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,6 @@
-  // Your web app's Firebase configuration
+var trainsList = [];
 
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+var firebaseConfig = {
     apiKey: "AIzaSyCrqvDa47jwFdt7Udl2-Qi3f1LZsucXYHM",
     authDomain: "firstproject2-2bbb8.firebaseapp.com",
     databaseURL: "https://firstproject2-2bbb8.firebaseio.com",
@@ -10,11 +9,11 @@
     messagingSenderId: "518162345823",
     appId: "1:518162345823:web:76806d06a3119fa74290ea",
     measurementId: "G-Q454P9TNYX"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  database= firebase.database();
+database= firebase.database();
 
 $("#submit-train").on("click", function (event){
 
@@ -23,7 +22,7 @@ $("#submit-train").on("click", function (event){
     var trainName = $("#train-name").val().trim();
     var destination = $("#destination").val().trim();
     var firstTrain = $("#first-train").val().trim();
-    var trainInterval = $("#train-interval").val().trim();
+    var trainInterval = parseInt($("#train-interval").val().trim());
 
     var addTrain = {
         name: trainName,
@@ -32,7 +31,9 @@ $("#submit-train").on("click", function (event){
         interval: trainInterval
     }
 
+    if (str.indexOf(' ') !== -1){
     database.ref().push(addTrain);
+    }
 
     $("#train-name").val("");
     $("#destination").val("");
@@ -42,10 +43,21 @@ $("#submit-train").on("click", function (event){
 });
 
 database.ref().on("child_added", function(snapshot){
+
+    console.log(snapshot)
     var trainName = snapshot.val().name;
     var destiation = snapshot.val().loc;
     var firstTrain = snapshot.val().first;
     var trainInterval = parseInt(snapshot.val().interval);
+
+    var addTrain = {
+        name: trainName,
+        loc: destination,
+        first: firstTrain,
+        interval: trainInterval
+    }
+
+    trainsList.push(addTrain);
     
 
     var currentMinute = parseInt(moment().minute());
@@ -56,20 +68,34 @@ database.ref().on("child_added", function(snapshot){
     
     var nextTrain = moment().add(nextMinute-currentMinute, 'minutes').format('hh:mm');
 
-    console.log(currentMinute);
-    console.log(nextMinute);
-    
     var newTrow = $("<tr>").append( 
         $("<td>").text(trainName),
         $("<td>").text(destiation),
         $("<td>").text(trainInterval),
-        $("<td>").text(nextTrain),
-        $("<td>").text(nextMinute - currentMinute)
+        $("<td>").text(nextTrain).addClass(trainName +"-next"),
+        $("<td>").text(nextMinute - currentMinute).addClass(trainName+"-left")
     );
-
-    
 
     $("#train-list").append(newTrow);
 
 });
 
+setInterval(function(){
+    for (i = 0; i<trainsList.length; i++){
+        var currentMinute = parseInt(moment().minute());
+        var nextMinute = parseInt(trainsList[i].interval);
+        
+        while(currentMinute > nextMinute){
+            nextMinute += trainsList[i].interval;
+        }
+
+        var nextTrain = moment().add(nextMinute-currentMinute, 'minutes').format('hh:mm');
+            
+        $("."+trainsList[i].name + "-next").text(nextTrain);
+        if(nextMinute-currentMinute === 0){
+            $("."+trainsList[i].name + "-left").text("Now Boarding");
+        }else{
+            $("."+trainsList[i].name + "-left").text(nextMinute - currentMinute)
+        }
+    }
+}, 1000);
